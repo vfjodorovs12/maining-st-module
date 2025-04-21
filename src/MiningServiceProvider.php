22bcd3b4dc028056fbf50165bc151bf634157b8b
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-04-21 18:24:15 +0100

    Update MiningServiceProvider.php

--- src/MiningServiceProvider.php
+++ src/MiningServiceProvider.php
@@ -13,13 +13,29 @@ class MiningServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/Http/routes.php');
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'mining');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->mergeConfigFrom(__DIR__.'/Config/mining.config.php', 'mining');
+        // Загрузка маршрутов
+        $this->loadRoutesFrom(__DIR__ . '/Http/routes.php');
 
+        // Загрузка представлений
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'mining');
+
+        // Загрузка миграций
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        // Загрузка конфигурации
+        $this->mergeConfigFrom(__DIR__ . '/Config/mining.config.php', 'mining');
+
+        // Публикация конфигурации
         $this->publishes([
-            __DIR__.'/Config/mining.config.php' => config_path('mining.php'),
+            __DIR__ . '/Config/mining.config.php' => config_path('mining.php'),
         ], 'config');
+
+        // Добавление переводов
+        $this->loadTranslationsFrom(__DIR__ . '/resources/lang', 'mining');
+
+        // Публикация переводов
+        $this->publishes([
+            __DIR__ . '/resources/lang' => resource_path('lang/vendor/mining'),
+        ], 'translations');
     }
 }
